7deec2ae4aabd871f4b8e42b36e8996840f1734f
jargdev <fallden4@humanoid.net>
2025-10-13 01:28:04 -0400

    add portfolio card

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,16 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    server: {
+      allowedHosts: true,
+      host: true,
+      port: 4321,
+      hmr: {
+        protocol: 'wss',
+        host: 'apiv4321.skibidihomer.fun',
+        port: 4321
+      }
+    }
   }
 });
